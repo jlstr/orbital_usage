4409adbf5faebb6fc71b6f677580bd8acc015f4a
Jose Estrella Campana <jlecampana@gmail.com>
2025-06-16 17:59:48 +0100

    Add Unit Tests for Pydantic Models

--- src/orbital_usage_api/main.py
+++ src/orbital_usage_api/main.py
@@ -2,9 +2,14 @@ from fastapi import FastAPI
 from src.orbital_usage_api.models import UsageResponse
 from src.orbital_usage_api.usage_logic import compute_usage_report
 
+# Main FastAPI Object used to expose endpoints
 app = FastAPI()
 
 # API's /usage endpoint
 @app.get("/usage", response_model = UsageResponse)
 async def calculate_usage():
   return await compute_usage_report()
+
+@app.get("/")
+def root():
+  return { message: "Welcome", success: True }
